a63b6a33f9e40f1a1c2691b0e0b7299563c524a0
Omar Zeidan <o.zeidan97@gmail.com>
2025-06-26 14:35:03 +0200

    Remove unnecessary table indices on file_info

--- migrations/000005_create_files_table.up.sql
+++ migrations/000005_create_files_table.up.sql
@@ -7,6 +7,3 @@ CREATE TABLE file_info (
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
-
-CREATE INDEX idx_file_info_linked_resource ON file_info(linked_resource_type, linked_resource_id);
-CREATE INDEX idx_file_info_file_type ON file_info(file_type); 
